897a6ce051f8c772bcb81e7966dbc2cc84b8a354
FaryalKhwaja <iamfaryalkhwaja@gmail.com>
2025-07-05 19:25:06 +0500

    Add style

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ button.addEventListener("click", () => {
 let listItem = document.createElement("li");
 let listText = document.createElement("span");
 let listBtn = document.createElement("button");
+listBtn.style.padding = '5px';
+listBtn.style.margin = "3px";
+listBtn.style.border = "none";
+listBtn.style.borderRadius = "3px";
+listBtn.style.backgroundColor = "light grey";
+
 
 listText.textContent = myItem;
 listBtn.textContent = "delete";
